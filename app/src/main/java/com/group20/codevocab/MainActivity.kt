7d2be79997dff4b7e9d778d34c9b18b0c83481e2
Hoàng Hồ Quốc Bảo <22520102@gm.uit.edu.vn>
2025-12-19 11:24:33 +0700

    update bottom menu

--- app/src/main/java/com/group20/codevocab/MainActivity.kt
+++ app/src/main/java/com/group20/codevocab/MainActivity.kt
@@ -1,13 +1,11 @@
 package com.group20.codevocab
 
-import android.content.Context
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.group20.codevocab.databinding.ActivityMainBinding
-import com.group20.codevocab.utils.LocaleHelper
 
 class MainActivity : AppCompatActivity() {
 
@@ -15,25 +13,15 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        // Dùng ViewBinding để inflate layout XML
+
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        // Lấy NavController từ NavHostFragment
-//        val navController = findNavController(R.id.nav_host_fragment)
+        val navView: BottomNavigationView = binding.navView
 
-        // Lấy NavHostFragment từ FragmentManager
-        val navHostFragment = supportFragmentManager
-            .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         val navController = navHostFragment.navController
-
-        // Kết nối BottomNavigationView với NavController
-        binding.bottomNav.setupWithNavController(navController)
-    }
-
-    override fun attachBaseContext(newBase: Context?) {
-        val lang = LocaleHelper.getLanguage(newBase!!)
-        val context = LocaleHelper.setLocale(newBase, lang)
-        super.attachBaseContext(context)
+        
+        navView.setupWithNavController(navController)
     }
 }
